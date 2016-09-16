0110998a63741c9da9fcd09bbf0b2d1f3ee929ae
3767Jack <3767jack@gmail.com>
2016-09-16 13:00:44 -0400

    Add files via upload

---
+++ Testing-Website/Coding Bleh/javascript/updatelog.js
@@ -0,0 +1,12 @@
+function UpdateLog(){
+
+	console.log("Update Log:" + "The biggest update release yet. Update 1.0 Beta features an update to the Twitter Chat feature. Now you only have to type in the box to get the wanted tweet and press submit. Text fixes within the twitter chat have also been fixed. "
+				+ "A complete change to the website main page, new background image and font. "
+				+ "CSS code in development "
+				+ "New pages for the site are being worked on. "
+				+ "New text on the front page. "
+				+ "Welcome to the page image pop up is being worked on."
+		)
+
+	alert("Check console for Update Log.")
+}
