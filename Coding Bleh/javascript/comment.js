59cde61f1b0e759d204e60a36a1fa6b7033140c2
3767Jack <3767jack@gmail.com>
2016-12-16 12:20:44 -0500

    Add files via upload

--- Coding Bleh/javascript/comment.js
+++ Coding Bleh/javascript/comment.js
@@ -1,9 +1,8 @@
 function writeUserData() {
 	var userId = firebase.auth().currentUser.uid;
-
+	var text = document.getElementById('commentSub')
   firebase.database().ref('comments/' + userId).set({
     author: userId,
     text: text,
-    profile_picture : imageUrl
   });
 }
